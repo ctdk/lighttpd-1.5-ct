f8a056bac6f56c0a66e4b7df11f3134abc885a71
Jan Kneschke <jan@kneschke.de>
2007-02-03 20:42:26 +0000

    fixed compile on win32
    git-svn-id: svn://svn.lighttpd.net/lighttpd/trunk@1592 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/network_gthread_aio.c
+++ src/network_gthread_aio.c
@@ -4,18 +4,11 @@
 #include "network_backends.h"
 #ifdef USE_GTHREAD
 #include <sys/types.h>
-#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/time.h>
-#include <sys/resource.h>
-
-#include <netinet/in.h>
-#include <netinet/tcp.h>
 
 #include <errno.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <netdb.h>
 #include <string.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -28,6 +21,7 @@
 #include "joblist.h"
 
 #include "sys-files.h"
+#include "sys-socket.h"
 
 typedef struct {
 	chunk *c;

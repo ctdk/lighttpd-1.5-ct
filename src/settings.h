2e4d41dae2e0da7b27a6fef624c04991d507dd7b
Jan Kneschke <jan@kneschke.de>
2007-04-12 17:56:49 +0000

    added the UNUSED_PARAM macro
    git-svn-id: svn://svn.lighttpd.net/lighttpd/trunk@1748 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/settings.h
+++ src/settings.h
@@ -150,6 +150,14 @@ typedef enum { HANDLER_UNSET,
   #define LI_EXCEPTIONAPI(api)
 #endif
 
+#ifdef UNUSED_PARAM
+#elif defined(__GNUC__)
+# define UNUSED_PARAM(x) UNUSED_ ## x __attribute__((unused))
+#elif defined(__LCLINT__)
+# define UNUSED_PARAM(x) /*@unused@*/ x
+#else
+# define UNUSED_PARAM(x) x
+#endif
 
 
 #endif

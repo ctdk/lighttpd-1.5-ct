4f9dcbbb8bde66845888b3d757a3e9d8adaaa417
Jan Kneschke <jan@kneschke.de>
2007-04-18 12:46:19 +0000

    suppress the stderr on a failing "which"
    git-svn-id: svn://svn.lighttpd.net/lighttpd/trunk@1801 152afb58-edef-0310-8abb-c4023f1b3aa9

--- autogen.sh
+++ autogen.sh
@@ -23,49 +23,57 @@ set -e
 
 ## on macosx glibtoolize, others have libtool
 if test x$LIBTOOLIZE = x; then
-  if test \! x`which glibtoolize` = x; then
+  if test \! x`which glibtoolize 2> /dev/null` = x; then
     LIBTOOLIZE=glibtoolize
-  elif test \! x`which libtoolize-1.5` = x; then
+  elif test \! x`which libtoolize-1.5 2> /dev/null` = x; then
     LIBTOOLIZE=libtoolize-1.5
-  elif test \! x`which libtoolize` = x; then
+  elif test \! x`which libtoolize 2> /dev/null` = x; then
     LIBTOOLIZE=libtoolize
+  else 
+    echo "libtoolize 1.5.x wasn't found, exiting"; exit 0
   fi
 fi
 
 ## suse has aclocal and aclocal-1.9
 if test x$ACLOCAL = x; then
-  if test \! x`which aclocal-1.9` = x; then
+  if test \! x`which aclocal-1.9 2> /dev/null` = x; then
     ACLOCAL=aclocal-1.9
-    AUTOMAKE=automake-1.9
-  elif test \! x`which aclocal` = x; then
+  elif test \! x`which aclocal 2> /dev/null` = x; then
     ACLOCAL=aclocal
-    AUTOMAKE=automake
+  else 
+    echo "automake 1.9.x (aclocal) wasn't found, exiting"; exit 0
   fi
 fi
 
 if test x$AUTOMAKE = x; then
-  if test \! x`which automake-1.9` = x; then
+  if test \! x`which automake-1.9 2> /dev/null` = x; then
     AUTOMAKE=automake-1.9
-  elif test \! x`which automake` = x; then
+  elif test \! x`which automake 2> /dev/null` = x; then
     AUTOMAKE=automake
+  else 
+    echo "automake 1.9.x wasn't found, exiting"; exit 0
   fi
 fi
 
 
 ## macosx has autoconf-2.59 and autoconf-2.60
 if test x$AUTOCONF = x; then
-  if test \! x`which autoconf-2.59` = x; then
+  if test \! x`which autoconf-2.59 2> /dev/null` = x; then
     AUTOCONF=autoconf-2.59
-  elif test \! x`which autoconf` = x; then
+  elif test \! x`which autoconf 2> /dev/null` = x; then
     AUTOCONF=autoconf
+  else 
+    echo "autoconf 2.59+ wasn't found, exiting"; exit 0
   fi
 fi
 
 if test x$AUTOHEADER = x; then
-  if test \! x`which autoheader-2.59` = x; then
+  if test \! x`which autoheader-2.59 2> /dev/null` = x; then
     AUTOHEADER=autoheader-2.59
-  elif test \! x`which autoheader` = x; then
+  elif test \! x`which autoheader 2> /dev/null` = x; then
     AUTOHEADER=autoheader
+  else 
+    echo "autoconf 2.59+ (autoheader) wasn't found, exiting"; exit 0
   fi
 fi
 

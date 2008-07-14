d5a92e59983589acde4c3980f08202b78fb83a9a
Elan Ruusamäe <glen@delfi.ee>
2008-07-14 15:45:43 +0000

    - let spawn-fcgi propagate exit code from spawned fcgi application
    git-svn-id: svn://svn.lighttpd.net/lighttpd/trunk@2199 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/spawn-fcgi.c
+++ src/spawn-fcgi.c
@@ -43,7 +43,7 @@ typedef int socklen_t;
 #ifdef HAVE_SYS_UN_H
 int fcgi_spawn_connection(char *appPath, char **appArgv, char *addr, unsigned short port, const char *unixsocket, int fork_count, int child_count, int pid_fd, int nofork) {
 	int fcgi_fd;
-	int socket_type, status;
+	int socket_type, status, rc = 0;
 	struct timeval tv = { 0, 100 * 1000 };
 
 	struct sockaddr_un fcgi_addr_un;
@@ -231,14 +231,17 @@ int fcgi_spawn_connection(char *appPath, char **appArgv, char *addr, unsigned sh
 					if (WIFEXITED(status)) {
 						fprintf(stderr, "%s.%d: child exited with: %d\n",
 							__FILE__, __LINE__, WEXITSTATUS(status));
+						rc = WEXITSTATUS(status);
 					} else if (WIFSIGNALED(status)) {
 						fprintf(stderr, "%s.%d: child signaled: %d\n",
 							__FILE__, __LINE__,
 							WTERMSIG(status));
+						rc = 1;
 					} else {
 						fprintf(stderr, "%s.%d: child died somehow: %d\n",
 							__FILE__, __LINE__,
 							status);
+						rc = status;
 					}
 				}
 
@@ -255,7 +258,7 @@ int fcgi_spawn_connection(char *appPath, char **appArgv, char *addr, unsigned sh
 
 	close(fcgi_fd);
 
-	return 0;
+	return rc;
 }
 
 

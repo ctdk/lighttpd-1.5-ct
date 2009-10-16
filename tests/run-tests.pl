6022b12ee24438a07cacd2797cfddc26b3e3a828
Stefan Bühler <stbuehler@web.de>
2009-10-16 18:54:10 +0000

    tests: improve test system (RUNTESTS, bt full)
    git-svn-id: svn://svn.lighttpd.net/lighttpd/trunk@2663 152afb58-edef-0310-8abb-c4023f1b3aa9

--- tests/run-tests.pl
+++ tests/run-tests.pl
@@ -2,18 +2,19 @@
 
 use strict;
 
-use Test::Harness qw(&runtests $verbose); 
+use Test::Harness qw(&runtests $verbose);
 $verbose = (defined $ENV{'VERBOSE'} ? $ENV{'VERBOSE'} : 0);
+my $tests = (defined $ENV{'RUNTESTS'} ? $ENV{'RUNTESTS'} : '');
 
 my $srcdir = (defined $ENV{'srcdir'} ? $ENV{'srcdir'} : '.');
 
 opendir DIR, $srcdir;
 my (@fs, $f);
 while ($f = readdir(DIR)) {
-	if ($f =~ /\.t$/) {
+	if ($f =~ /^(.*)\.t$/) {
+		next if ($tests ne '' and $tests !~ /(^|\s+)$1(\s+|$)/);
 		push @fs, $srcdir.'/'.$f;
 	}
 }
 closedir DIR;
 runtests @fs;
-
